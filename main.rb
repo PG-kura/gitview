bf690227d6cabe4ffa745810d16ccfc32f40036a
PG_kura <kura@MacBookAir.local>
2012-07-06 01:08:55 +0900

    use Gemfile.

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 $:.unshift File.join(File.dirname(__FILE__), '.')
 require 'rubygems'
-require 'example'
+# require 'example'
 
 require 'sinatra'
 require 'haml'
@@ -27,26 +27,26 @@ get '/pjax.js' do
 end
 
 get '/' do
-  @v = 1
   haml :index
-  #e = Example::Example.new
-  #e.toUpper('aaa bbb ccc')
 end
 
 get '/1' do
-  if params[:_pjax].nil?
-    puts "### pjax is null"
-  elsif params[:_pjax] == ""
-    puts "### pjax is empty"
-  else
-    puts "### pjax is something"
-  end
   if params[:_pjax].nil? or params[:_pjax] == ""
     @partial = :first
     haml :default_layout
   else
-    puts `git status`
     haml :first
   end
 end
+
+
+
+#get '/' do
+#  @v = 1
+#  haml :index
+#  e = Example::Example.new
+#  e.toUpper('aaa bbb ccc')
+#end
+
+
 
